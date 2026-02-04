f78a577025c61200f8d0f2884c7ebb5021b5e8e2
Alejandro Hernández <alejandro1998hernandez@gmail.com>
2026-02-04 03:19:54 +0100

    Add Redux store and user slice with CRUD operations

---
+++ projects/08_CRUD_react_redux/src/store/index.ts
@@ -0,0 +1,36 @@
+import type { Middleware, PayloadAction } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
+import { syncDB } from "../services/syncDB";
+import usersReducer from "./slices/users";
+
+const persistantMiddleware: Middleware = (store) => (next) => (action) => {
+	const { type } = action as PayloadAction;
+	next(action);
+	if (type.startsWith("users/")) {
+		localStorage.setItem("users", JSON.stringify(store.getState().users));
+	}
+};
+
+const syncWithDatabaseMiddleware: Middleware =
+	(store) => (next) => (action) => {
+		const { type, payload } = action as PayloadAction;
+		// Aquí iría la lógica para sincronizar con una base de datos remota
+		next(action);
+		if (type.startsWith("users/")) {
+			// Simulación de sincronización
+			syncDB(type, payload);
+		}
+	};
+
+export const store = configureStore({
+	reducer: {
+		users: usersReducer,
+	},
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware().concat(
+			persistantMiddleware,
+			syncWithDatabaseMiddleware,
+		),
+});
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
