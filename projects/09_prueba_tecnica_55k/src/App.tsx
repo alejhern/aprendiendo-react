d431c0c1bff6a2b66a6d8cbc5ea9ce0a4e137274
Alejandro Hernández <alejandro1998hernandez@gmail.com>
2026-02-05 21:36:01 +0100

    Refactor App and TableUsers components to utilize useUsers hook for improved user data management and loading states; remove unnecessary state and handlers.

--- projects/09_prueba_tecnica_55k/src/App.tsx
+++ projects/09_prueba_tecnica_55k/src/App.tsx
@@ -1,122 +1,21 @@
-import { useEffect, useId, useMemo, useRef, useState } from "react";
-import { Button, ButtonGroup, Container, Form, Row } from "react-bootstrap";
+import { Button, Container, Row } from "react-bootstrap";
 import "./App.css";
 import { TableUsers } from "./components/TableUsers";
-import { fetchUsers } from "./services/fetchUser";
-import { SortBy, type User } from "./types.d";
+import { useUsers } from "./hooks/useUsers";
 
 function App() {
-	const [users, setUsers] = useState<User[]>([]);
-	const [striped, setStriped] = useState<boolean>(false);
-	const [sorting, setSorting] = useState<SortBy>(SortBy.NONE);
-	const [filterCountry, setFilterCountry] = useState<string>("");
-	const originalUsers = useRef<User[]>([]);
-	const [loading, setLoading] = useState<boolean>(false);
-	const [currentPage, setCurrentPage] = useState<number>(1);
-
-	useEffect(() => {
-		setLoading(true);
-		(async () => {
-			try {
-				const newUsers = await fetchUsers(currentPage);
-				if (currentPage === 1) {
-					originalUsers.current = newUsers;
-				}
-				setUsers((prevUsers) => [...prevUsers, ...newUsers]);
-			} catch (error) {
-				console.error("Error fetching users:", error);
-			} finally {
-				setLoading(false);
-			}
-		})();
-	}, [currentPage]);
-
-	const filteredUsers = useMemo(
-		() =>
-			typeof filterCountry === "string" && filterCountry.length > 0
-				? users.filter((user) =>
-						user.location.country
-							.toLowerCase()
-							.includes(filterCountry.toLowerCase()),
-				  )
-				: users,
-		[filterCountry, users],
-	);
-
-	const sortedUsers = useMemo(() => {
-		if (sorting === SortBy.NONE) return filteredUsers;
-		const compareProperties: Record<string, (user: User) => string> = {
-			[SortBy.COUNTRY]: (user) => user.location.country,
-			[SortBy.NAME]: (user) => user.name.first,
-			[SortBy.LAST_NAME]: (user) => user.name.last,
-			[SortBy.EMAIL]: (user) => user.email,
-		};
-		return filteredUsers.toSorted((a, b) => {
-			const extractProp = compareProperties[sorting];
-			return extractProp(a).localeCompare(extractProp(b));
-		});
-	}, [sorting, filteredUsers]);
-
-	const toggleStriped = () => {
-		setStriped(!striped);
-	};
-
-	const toggleSorting = (newSorting: SortBy) => {
-		if (sorting === newSorting) return setSorting(SortBy.NONE);
-		return setSorting(newSorting);
-	};
-
-	const handlerDeleteUser = (email: string) => {
-		const filteredUsers = users.filter((user) => user.email !== email);
-		setUsers(filteredUsers);
-	};
+	const { isLoading, fetchNextPage, hasNextPage } = useUsers();
 	return (
 		<Container fluid>
 			<Row className="my-3">
 				<h1>Prueva Tecnica</h1>
 			</Row>
-			<Form>
-				<Form.Group className="mb-3" controlId={useId()}>
-					<Form.Label>Search by country</Form.Label>
-					<Form.Control
-						type="text"
-						placeholder="Spain, France, Germany..."
-						onChange={(e) => setFilterCountry(e.target.value)}
-					/>
-				</Form.Group>
-			</Form>
-			<Row className="mb-3">
-				<ButtonGroup aria-label="Basic example">
-					<Button variant="secondary" onClick={toggleStriped}>
-						Toggle Striped
-					</Button>
-					<Button
-						variant="secondary"
-						onClick={() => toggleSorting(SortBy.COUNTRY)}
-					>
-						{sorting !== SortBy.NONE ? "Unsort" : "Sort"} by country
-					</Button>
-					<Button
-						variant="secondary"
-						onClick={() => setUsers(originalUsers.current)}
-					>
-						Restore Users
-					</Button>
-				</ButtonGroup>
-			</Row>
-			<Row>
-				<TableUsers
-					users={sortedUsers}
-					striped={striped}
-					deleteUser={handlerDeleteUser}
-					toggleSorting={toggleSorting}
-					loading={loading}
-				/>
-			</Row>
+			<TableUsers />
 			<Row>
 				<Button
-					variant="primary"
-					onClick={() => setCurrentPage((prev) => prev + 1)}
+					variant="secondary"
+					onClick={() => void fetchNextPage()}
+					disabled={!hasNextPage || isLoading}
 				>
 					Load More
 				</Button>
--- projects/09_prueba_tecnica_55k/src/components/TableUsers.tsx
+++ projects/09_prueba_tecnica_55k/src/components/TableUsers.tsx
@@ -1,13 +1,14 @@
-import { Button, Spinner, Table } from "react-bootstrap";
+import { useCallback, useId, useState } from "react";
+import {
+	Button,
+	ButtonGroup,
+	Form,
+	Row,
+	Spinner,
+	Table,
+} from "react-bootstrap";
+import { useUsers } from "../hooks/useUsers";
 import { SortBy, type User } from "../types.d";
-
-type TableUsersProps = {
-	users: User[];
-	striped: boolean;
-	loading: boolean;
-	deleteUser: (email: string) => void;
-	toggleSorting: (newSorting: SortBy) => void;
-};
 
 function SorteableTableHeader({
 	toggleSorting,
@@ -60,10 +61,8 @@ function SorteableTableHeader({
 	);
 }
 
-function RowUser({
-	user,
-	deleteUser,
-}: { user: User; deleteUser: (email: string) => void }) {
+function RowUser({ user }: { user: User }) {
+	const { handlerDeleteUser } = useUsers();
 	return (
 		<tr key={user.email}>
 			<td>
@@ -80,7 +79,7 @@ function RowUser({
 				<Button
 					variant="danger"
 					size="lg"
-					onClick={() => deleteUser(user.email)}
+					onClick={() => handlerDeleteUser(user.email)}
 				>
 					Delete
 				</Button>
@@ -89,35 +88,89 @@ function RowUser({
 	);
 }
 
-export function TableUsers({
-	users,
-	striped,
-	deleteUser,
+function FormTable({
+	toggleStriped,
+	sorting,
+	setFilterCountry,
 	toggleSorting,
-	loading,
-}: TableUsersProps) {
+}: {
+	toggleStriped: () => void;
+	sorting: SortBy;
+	setFilterCountry: (country: string) => void;
+	toggleSorting: (newSorting: SortBy) => void;
+}) {
+	const { refetch } = useUsers();
 	return (
-		<Table striped={striped} bordered hover size="lg">
-			<SorteableTableHeader toggleSorting={toggleSorting} />
-			<tbody>
-				{users.map((user) => (
-					<RowUser key={user.email} user={user} deleteUser={deleteUser} />
-				))}
-				{loading && (
-					<tr>
-						<td colSpan={6} className="text-center">
-							<Spinner animation="border" role="status" /> Loading...
-						</td>
-					</tr>
-				)}
-				{!loading && users.length === 0 && (
-					<tr>
-						<td colSpan={6} className="text-center">
-							No users found.
-						</td>
-					</tr>
-				)}
-			</tbody>
-		</Table>
+		<>
+			<Form>
+				<Form.Group className="mb-3" controlId={useId()}>
+					<Form.Label>Search by country</Form.Label>
+					<Form.Control
+						type="text"
+						placeholder="Spain, France, Germany..."
+						onChange={(e) => setFilterCountry(e.target.value)}
+					/>
+				</Form.Group>
+			</Form>
+			<Row className="mb-3">
+				<ButtonGroup aria-label="Basic example">
+					<Button variant="secondary" onClick={toggleStriped}>
+						Toggle Striped
+					</Button>
+					<Button
+						variant="secondary"
+						onClick={() => toggleSorting(SortBy.COUNTRY)}
+					>
+						{sorting !== SortBy.NONE ? "Unsort" : "Sort"} by country
+					</Button>
+					<Button variant="secondary" onClick={() => refetch()}>
+						Restore Users
+					</Button>
+				</ButtonGroup>
+			</Row>
+		</>
+	);
+}
+
+export function TableUsers() {
+	const { toggleSorting, isLoading, users, sorting, setFilterCountry } =
+		useUsers();
+	const [striped, setStriped] = useState<boolean>(false);
+
+	const toggleStriped = useCallback(() => {
+		setStriped(!striped);
+	}, [striped]);
+
+	return (
+		<>
+			<FormTable
+				toggleStriped={toggleStriped}
+				toggleSorting={toggleSorting}
+				setFilterCountry={setFilterCountry}
+				sorting={sorting}
+			/>
+			<Table striped={striped} bordered hover size="lg">
+				<SorteableTableHeader toggleSorting={toggleSorting} />
+				<tbody>
+					{users.map((user) => (
+						<RowUser key={user.email} user={user} />
+					))}
+					{isLoading && (
+						<tr>
+							<td colSpan={6} className="text-center">
+								<Spinner animation="border" role="status" /> Loading...
+							</td>
+						</tr>
+					)}
+					{!isLoading && users.length === 0 && (
+						<tr>
+							<td colSpan={6} className="text-center">
+								No users found.
+							</td>
+						</tr>
+					)}
+				</tbody>
+			</Table>
+		</>
 	);
 }
