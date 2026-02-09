a30178fef3f0fdf1309166f0bcd0f9df89e3a442
Alejandro Hernández <alejandro1998hernandez@gmail.com>
2026-02-09 20:33:10 +0100

    Refactor user management to use UserWithId type consistently and update user ID handling

--- projects/08_CRUD_react_redux/src/hooks/useModalUser.ts
+++ projects/08_CRUD_react_redux/src/hooks/useModalUser.ts
@@ -1,49 +1,27 @@
 import { useState } from "react";
-import { useAppSelector } from "../hooks/useStore";
 import { useUsersActions } from "../hooks/useUsersActions";
-import type { User } from "../types";
+import type { UserWithId } from "../types";
 
-export function useModalUser(userId?: number) {
+export function useModalUser(userId?: string) {
 	const [show, setShow] = useState(false);
 	const { addUser, editUser } = useUsersActions();
-	const users = useAppSelector((state) => state.users);
-	const user: User =
-		userId === undefined
-			? {
-					name: "",
-					email: "",
-					github: "",
-					image: "",
-			  }
-			: users.find((user) => user.id === userId) ?? {
-					name: "",
-					email: "",
-					github: "",
-					image: "",
-			  };
 
 	const handleClose = () => setShow(false);
 	const handleShow = () => setShow(true);
 
-	const handlerSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+	const handlerSubmit = (
+		e: React.FormEvent<HTMLFormElement>,
+		formData: UserWithId,
+	) => {
 		e.preventDefault();
-		const formData = new FormData(e.currentTarget);
-
-		const newUser: User = {
-			name: formData.get("name") as string,
-			email: formData.get("email") as string,
-			github: formData.get("github") as string,
-			image: formData.get("image") as string,
-		};
-
 		if (userId === undefined) {
-			addUser(newUser);
+			addUser(formData);
 		} else {
-			editUser({ ...newUser, id: userId });
+			editUser(formData);
 		}
 
 		setShow(false);
 	};
 
-	return { show, handleClose, handleShow, handlerSubmit, user };
+	return { show, handleClose, handleShow, handlerSubmit };
 }
