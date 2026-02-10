ad4e2ea0e26816f99ff3653ed9121c35122c1dc8
Alejandro Hernández <alejandro1998hernandez@gmail.com>
2026-02-10 12:30:16 +0100

    Refactor ModalUser and useModalUser for improved form handling and state management

--- projects/10_CRUD_QueryReact/src/components/ModalUser.tsx
+++ projects/10_CRUD_QueryReact/src/components/ModalUser.tsx
@@ -3,14 +3,51 @@ import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
 import PersonAddAlt1RoundedIcon from "@mui/icons-material/PersonAddAlt1Rounded";
 import SaveAltRoundedIcon from "@mui/icons-material/SaveAltRounded";
 import { Button as MuiButton } from "@mui/material";
-import { useId } from "react";
+import { useEffect, useId, useState } from "react";
 import Form from "react-bootstrap/Form";
 import Modal from "react-bootstrap/Modal";
 import { useModalUser } from "../hooks/useModalUser";
+import type { User } from "../types";
 
 export function ModalUser({ userId }: { userId?: number }) {
-	const { show, handleClose, handleShow, handlerSubmit, user } =
+	const { show, handleClose, handleShow, handlerSubmit, users } =
 		useModalUser(userId);
+	const nameId = useId();
+	const emailId = useId();
+	const githubId = useId();
+	const imageId = useId();
+
+	const [formData, setFormData] = useState<User>({
+		name: "",
+		email: "",
+		github: "",
+		image: "",
+	});
+
+	useEffect(() => {
+		if (!show && userId === undefined) {
+			setFormData({
+				name: "",
+				email: "",
+				github: "",
+				image: "",
+			});
+		}
+	}, [show, userId]);
+
+	useEffect(() => {
+		if (!show || userId === undefined) return;
+
+		const user = users?.find((user) => user.id === userId);
+		if (!user) return;
+
+		setFormData({
+			name: user.name,
+			email: user.email,
+			github: user.github,
+			image: user.image,
+		});
+	}, [show, userId, users]);
 
 	const iconOppenModal =
 		userId === undefined ? (
@@ -34,47 +71,59 @@ export function ModalUser({ userId }: { userId?: number }) {
 				backdrop="static"
 				keyboard={false}
 			>
-				<Form onSubmit={handlerSubmit}>
+				<Form onSubmit={(e) => handlerSubmit(e, formData)}>
 					<Modal.Header closeButton>
 						<Modal.Title>
 							{userId === undefined ? "Add User" : "Edit User"}
 						</Modal.Title>
 					</Modal.Header>
 					<Modal.Body>
-						<Form.Group className="mb-3" controlId={useId()}>
+						<Form.Group className="mb-3" controlId={nameId}>
 							<Form.Label>Name</Form.Label>
 							<Form.Control
 								type="text"
 								name="name"
 								placeholder="Enter name"
-								defaultValue={user.name}
+								value={formData.name}
+								onChange={(e) =>
+									setFormData({ ...formData, name: e.target.value })
+								}
 							/>
 						</Form.Group>
-						<Form.Group className="mb-3" controlId={useId()}>
+						<Form.Group className="mb-3" controlId={emailId}>
 							<Form.Label>Email</Form.Label>
 							<Form.Control
 								type="email"
 								name="email"
 								placeholder="Enter email"
-								defaultValue={user.email}
+								value={formData.email}
+								onChange={(e) =>
+									setFormData({ ...formData, email: e.target.value })
+								}
 							/>
 						</Form.Group>
-						<Form.Group className="mb-3" controlId={useId()}>
+						<Form.Group className="mb-3" controlId={githubId}>
 							<Form.Label>GitHub</Form.Label>
 							<Form.Control
 								name="github"
 								type="text"
 								placeholder="Enter GitHub username"
-								defaultValue={user.github}
+								value={formData.github}
+								onChange={(e) =>
+									setFormData({ ...formData, github: e.target.value })
+								}
 							/>
 						</Form.Group>
-						<Form.Group className="mb-3" controlId={useId()}>
+						<Form.Group className="mb-3" controlId={imageId}>
 							<Form.Label>Image URL</Form.Label>
 							<Form.Control
 								name="image"
 								type="text"
 								placeholder="Enter image URL"
-								defaultValue={user.image}
+								value={formData.image}
+								onChange={(e) =>
+									setFormData({ ...formData, image: e.target.value })
+								}
 							/>
 						</Form.Group>
 					</Modal.Body>
--- projects/10_CRUD_QueryReact/src/hooks/useModalUser.ts
+++ projects/10_CRUD_QueryReact/src/hooks/useModalUser.ts
@@ -1,44 +1,21 @@
 import { useCallback, useState } from "react";
-import type { User, UserWithId } from "../types";
+import type { User } from "../types";
 import { useUsers } from "./useUsers";
 
 export function useModalUser(userId?: number) {
 	const [show, setShow] = useState(false);
 	const { users, addUser, editUser } = useUsers();
-	const user: User =
-		userId === undefined
-			? {
-					name: "",
-					email: "",
-					github: "",
-					image: "",
-			  }
-			: users?.find((user: UserWithId) => user.id === userId) ?? {
-					name: "",
-					email: "",
-					github: "",
-					image: "",
-			  };
 
 	const handleClose = () => setShow(false);
 	const handleShow = () => setShow(true);
 
 	const handlerSubmit = useCallback(
-		(e: React.FormEvent<HTMLFormElement>) => {
+		(e: React.FormEvent<HTMLFormElement>, formData: User) => {
 			e.preventDefault();
-			const formData = new FormData(e.currentTarget);
-
-			const newUser: User = {
-				name: formData.get("name") as string,
-				email: formData.get("email") as string,
-				github: formData.get("github") as string,
-				image: formData.get("image") as string,
-			};
-
 			if (userId === undefined) {
-				addUser(newUser);
+				addUser(formData);
 			} else {
-				editUser({ ...newUser, id: userId });
+				editUser({ ...formData, id: userId });
 			}
 
 			setShow(false);
@@ -46,5 +23,5 @@ export function useModalUser(userId?: number) {
 		[userId, addUser, editUser],
 	);
 
-	return { show, handleClose, handleShow, handlerSubmit, user };
+	return { show, handleClose, handleShow, handlerSubmit, users };
 }
